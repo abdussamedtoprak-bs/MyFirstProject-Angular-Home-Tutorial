7aab9abbbfa2adb9b1f3f9f8303c7d9ad754e947
abdussamedtoprak-bs <abdussamedtoprak@gmail.com>
2026-07-28 16:06:56 +0300

    Training materials were added, and the application layout and content were updated.

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,14 +7,14 @@ import { Home } from './home/home';
   standalone: true,
   template: ` <main>
     <header class="brand-name">
-      <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true"/>
+      <img class="brand-logo" src="logo.svg" alt="logo" aria-hidden="true"/>
 
     </header>
   <section class="content">
     <app-home/>
   </section>
   
-  </main>`,
+  </main>`
   
   
 })
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -53,7 +53,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Acme Fresh Start Housing',
       city: 'Chicago',
       state: 'IL',
-      photo: '/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      photo: '/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
       availableUnits: 4,
       wifi: true,
       laundry: true
@@ -63,7 +63,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'A113 Transitional Housing',
       city: 'Santa Monica',
       state: 'CA',
-      photo: '/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg',
+      photo: 'brandon-griggs-wR11KBaB86U-unsplash.jpg',
       availableUnits: 0,
       wifi: false,
       laundry: true
@@ -73,7 +73,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Warm Beds Housing Support',
       city: 'Juneau',
       state: 'AK',
-      photo: '/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
+      photo: 'i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
       availableUnits: 1,
       wifi: false,
       laundry: false
@@ -83,7 +83,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Homesteady Housing',
       city: 'Chicago',
       state: 'IL',
-      photo: '/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
+      photo: 'ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
       availableUnits: 1,
       wifi: true,
       laundry: false
@@ -93,7 +93,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Happy Homes Group',
       city: 'Gary',
       state: 'IN',
-      photo: '/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
+      photo: 'krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
       availableUnits: 1,
       wifi: true,
       laundry: false
@@ -103,7 +103,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Hopeful Apartment Group',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
+      photo: 'r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
       availableUnits: 2,
       wifi: true,
       laundry: true
@@ -113,7 +113,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Seriously Safe Towns',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
+      photo: 'phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
       availableUnits: 5,
       wifi: true,
       laundry: true
@@ -123,7 +123,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Hopeful Housing Solutions',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/r-architecture-GGupkreKwxA-unsplash.jpg',
+      photo: 'r-architecture-GGupkreKwxA-unsplash.jpg',
       availableUnits: 2,
       wifi: true,
       laundry: true
@@ -133,7 +133,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Seriously Safe Towns',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
+      photo: 'saru-robert-9rP3mxf8qWI-unsplash.jpg',
       availableUnits: 10,
       wifi: false,
       laundry: false
@@ -143,7 +143,7 @@ housingLocationList: HousinglocationInfo[] = [
       name: 'Capital Safe Towns',
       city: 'Portland',
       state: 'OR',
-      photo: '/assets/webaliser-_TPTXZd9mOo-unsplash.jpg',
+      photo: 'webaliser-_TPTXZd9mOo-unsplash.jpg',
       availableUnits: 6,
       wifi: true,
       laundry: true
@@ -151,7 +151,7 @@ housingLocationList: HousinglocationInfo[] = [
 ];
 
 
-    readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+    readonly baseUrl = '/';
 
      housingLocation: HousinglocationInfo = {
     id: 9999,
