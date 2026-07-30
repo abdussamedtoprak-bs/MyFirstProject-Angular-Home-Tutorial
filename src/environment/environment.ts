3c1e0f74ccf376f5d66008e864ac2d24411a9da4
abdussamedtoprak-bs <abdussamedtoprak@gmail.com>
2026-07-30 16:50:27 +0300

    Firebase SDK and initial project setup

---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7xvWUjXU9X_ThUuJsdAeBmCTJxGD12qE",
+  authDomain: "angular-homes-first-projects.firebaseapp.com",
+  projectId: "angular-homes-first-projects",
+  storageBucket: "angular-homes-first-projects.firebasestorage.app",
+  messagingSenderId: "952386379290",
+  appId: "1:952386379290:web:c548927e5a69c461f7bba3"
+};
+
+const app = initializeApp(firebaseConfig);
